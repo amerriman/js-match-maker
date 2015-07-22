f574ce0e9fc57531e03948620dcd51ecf01a6fb2
amerriman <amm434@gmail.com>
2015-07-22 15:01:21 -0600

    descending into popup hell

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var numStudents = prompt("How many students are learning JavaScript?");
+    //var student = {};
 
 var studentArray = [];
 var addStudents = function(){
   if (numStudents > 0){
     for (var i = 0; i < numStudents; i++) {
-    var student = {};
+      var student = {};
       student.name = prompt("Student name?");
       student.phoneNumber = prompt("Student phone number?");
-      student.city = prompt("Student city");
-
+      student.city = prompt("Student city?");
         studentArray.push(student);
-    }  //return student;
+    }
   }
 };
 
@@ -24,14 +24,37 @@ var mentorArray = [];
 var addMentors = function(){
   for (var i = 0; i < numMentors; i++) {
   var mentor = {};
-    mentor.Name = prompt("Mentor name?");
-    mentor.PhoneNumber = prompt("Mentor phone number?");
-    mentor.City = prompt("Mentor city");
+    mentor.name = prompt("Mentor name?");
+    mentor.phoneNumber = prompt("Mentor phone number?");
+    mentor.city = prompt("Mentor city?");
 
       mentorArray.push(mentor);
-    }  //return student;
+    }
   };
 }
 
 addMentors();
 console.log(mentorArray);
+
+//var contactList = alert("There are " + numStudents + " students and " + numMentors + " mentors.");
+
+var studentList = ""
+
+for (var i = 0; i < studentArray.length; i++) {
+  var list = 'Name: ' + studentArray[i].name + '\nPhone Number: ' + studentArray[i].phoneNumber + '\nCity: ' + studentArray[i].city + '\n\n';
+
+    studentList += (list);
+
+}
+alert('There are ' + numStudents + ' students.\n' + studentList);
+
+var mentorList = ""
+
+for (var i = 0; i < mentorArray.length; i++) {
+  var mlist = 'Name: ' + mentorArray[i].name + '\nPhone Number: ' + mentorArray[i].phoneNumber + '\nCity: ' + mentorArray[i].city + '\n\n';
+
+    mentorList += (mlist);
+
+}
+alert('There are ' + numMentors + ' mentors.\n' + mentorList);
+
